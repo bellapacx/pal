5cffcac7bf7f62d10e9b8b2b4470544d4f45c309
sunvilacoin <sunvilacoin@gmail.com>
2025-09-14 16:50:19 +0300

    WIPssddkdssdjbsjdkgglffdFFDDFFxx

---
+++ app/ser/page.tsx
@@ -0,0 +1,10 @@
+// app/services/page.tsx
+import ServicesPanel from '../components/ServicesPanel'
+
+export default function ServicesPage() {
+  return (
+    <>
+      <ServicesPanel />
+    </>
+  )
+}
